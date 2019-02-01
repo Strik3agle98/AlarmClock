9f8b04f9e75556718aba36dee3fd899eec4f94ce
Unknown <can_saelo@hotmail.com>
2019-02-01 21:51:54 +0700

    DHT sensor integration

--- AlarmClock.ino
+++ AlarmClock.ino
@@ -2,37 +2,30 @@
 // Author: Md. Khairul Alam
 // Date: 5 July 2018
 
-#include <SPI.h>
-//#include <bitBangedSPI.h>
-//#include <MAX7219_Dot_Matrix.h>
-#include <ESP8266HTTPClient.h>
 #include "Arduino.h"
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 const byte chips = 8; //number of matrix in the display
+#define DHTPIN            14 //Pin connected to DHT
+// Uncomment the type of sensor in use:
+//#define DHTTYPE           DHT11     // DHT 11 
+#define DHTTYPE           DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE           DHT21     // DHT 21 (AM2301)
 
-// for NodeMCU 1.0
-#define DIN_PIN 15  // D8
-#define CS_PIN  13  // D7
-#define CLK_PIN 12  // D6
 
-//MAX7219_Dot_Matrix display (chips, CS_PIN, DIN_PIN, CLK_PIN);  // 2 chips, then specify the LOAD, DIN, CLK pins
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
 
 WiFiClient client;
 
-String weatherMain = "";
-String weatherDescription = "";
-String weatherLocation = "";
-String country;
 int humidity;
-int pressure;
 float temp;
-float tempMin, tempMax;
-int clouds;
-float windSpeed;
 String date;
 int h,m,s;
 int sunrise, sunset;
@@ -63,6 +56,10 @@ void setup ()
   lcd.begin(20, 4);
   lcd.setCursor(0,1);
   Serial.begin(115200);
+  dht.begin();
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  dht.humidity().getSensor(&sensor);
   Serial.print("Connecting WiFi ");
   lcd.print("Connecting");
   WiFi.begin(ssid, password);
@@ -81,7 +78,7 @@ void setup ()
   String str = weatherString;
   int str_len = str.length() + 1; 
   str.toCharArray(weather_message, str_len) ;   
-
+  updateTemp();
   getTime();
   
 }  // end of setup
@@ -90,60 +87,21 @@ unsigned long lastMoved = 0;
 unsigned long MOVE_INTERVAL = 2;  // mS
 int  messageOffset;
 
-//void updateDisplay (char *msg)
-//  {
-//  //String message = msg;
-//  display.sendSmooth (msg, messageOffset);
-//  
-//  // next time show one pixel onwards
-//  if (messageOffset++ >= (int) (strlen (msg) * 8))
-//    messageOffset = - chips * 8;
-//  }  // end of updateDisplay
   
 long last = millis();
 long lastTime = millis();
 
 void loop () 
   { 
-  if((millis()- last)>1000*60*15){  //update weather every 15 minutes 
-    //getWeatherData();
+   if((millis()- last)>1000*60*15){  //update weather every 15 minutes 
     getTime();
-    //String str = weatherString;
-    //int str_len = str.length() + 1;
-    //str.toCharArray(weather_message, str_len) ;
     last = millis();
    }
-   
-//  // update display if time is up
-//  if(s>0 && s<40){
-//  if (millis () - lastMoved >= MOVE_INTERVAL)
-//    {
-//      //updateDisplay(weather_message);
-//      lastMoved = millis ();
-//    }  
-//  }
-//
-//  if(s>40 && s<55){
-//  if (millis () - lastMoved >= MOVE_INTERVAL)
-//    {
-//      //updateDisplay(date_message);
-//      lastMoved = millis ();
-//    }  
-//  }
-//  /*
-//  // do other stuff here    
-//  if(millis()-lastTime>65000){ 
-//    long now = millis();
-//    do{
-//      updateTime();
-//      showTime();
-//      }while(millis()-now<40000);
-//    now = millis();
-//    lastTime = millis();
-//  }  
-//   */
- showTime(); 
-   
+   if(s%5==0){
+    updateTemp();
+   }
+
+ showData(); 
  updateTime();
 
  if(millis() < last) last = 0;
@@ -155,78 +113,7 @@ void loop ()
 
 //const char *weatherHost = "api.openweathermap.org";
 
-void getWeatherData()
-{
-//  Serial.print("connecting to "); Serial.println(weatherHost);
-//  if (client.connect(weatherHost, 80)) {
-//    client.println(String("GET /data/2.5/weather?id=") + cityID + "&units=metric&appid=" + weatherKey + weatherLang + "\r\n" +
-//                "Host: " + weatherHost + "\r\nUser-Agent: ArduinoWiFi/1.1\r\n" +
-//                "Connection: close\r\n\r\n");
-//  } else {
-//    Serial.println("connection failed");
-//    return;
-//  }
-//  String line;
-//  int repeatCounter = 0;
-//  while (!client.available() && repeatCounter < 10) {
-//    delay(500);
-//    Serial.println("w.");
-//    repeatCounter++;
-//  }
-//  while (client.connected() && client.available()) {
-//    char c = client.read(); 
-//    if (c == '[' || c == ']') c = ' ';
-//    line += c;
-//  }
-//  Serial.println(line);
-//  client.stop();
 
-  HTTPClient http;
-  http.begin("https://api.openweathermap.org/data/2.5/weather?id=1609350&units=metric&appid=280583189452d44eebf09fe2ad36b3f2");
-  int httpCode = http.GET(); //still doesn't work
-  Serial.print(httpCode);
-  Serial.print("Connecting to api.openweathermap.org");
-  while(httpCode <= 0){
-    Serial.print(".");
-    delay(500);
-  }
-  payload=http.getString();
-  Serial.println(payload);
-  http.end();
-  //get data from api
-  
-  DynamicJsonBuffer jsonBuf;
-  JsonObject &root = jsonBuf.parseObject(payload);
-  if (!root.success())
-  {
-    Serial.println("parseObject() failed");
-    return;
-  }
-  //weatherMain = root["weather"]["main"].as<String>();
-  weatherDescription = root["weather"]["description"].as<String>();
-  weatherDescription.toLowerCase();
-  Serial.println(weatherDescription);
-  //  weatherLocation = root["name"].as<String>();
-  //  country = root["sys"]["country"].as<String>();
-  sunrise = root["sys"]["sunrise"];
-  sunset = root["sys"]["sunset"];
-  temp = root["main"]["temp"];
-  humidity = root["main"]["humidity"];
-  pressure = root["main"]["pressure"];
-  tempMin = root["main"]["temp_min"];
-  tempMax = root["main"]["temp_max"];
-  windSpeed = root["wind"]["speed"];
-  clouds = root["clouds"]["all"];
-  //String deg = String(char('~')+131);
-  //weatherString = "  Temp: " + String(temp,1) /*+ deg*/ + "C (" + String(tempMin,1) /*+ deg*/ + "-" + String(tempMax,1) /*+ deg*/ + ")  ";
-  weatherString = "  Temp: " + String(temp,1) /*+ deg*/ + "C";
-  //weatherString += weatherDescription;
-  weatherString += "  Humidity: " + String(humidity) + "% ";
-  weatherString += "  Pressure: " + String(pressure) + "hPa ";
-  weatherString += "  Clouds: " + String(clouds) + "%  ";
-  weatherString += "  Wind: " + String(windSpeed,1) + "m/s   ";
-  Serial.println(weatherString);
-}
 
 
 // =======================================================================
@@ -277,7 +164,14 @@ void getTime()
 }
 
 // =======================================================================
-
+void updateTemp(){
+  sensors_event_t event;  
+  dht.temperature().getEvent(&event);
+  temp = event.temperature;
+  dht.humidity().getEvent(&event);
+  humidity = event.relative_humidity;
+}
+// =======================================================================
 void updateTime()
 {
   long curEpoch = localEpoc + ((millis() - localMillisAtUpdate) / 1000);
@@ -289,7 +183,7 @@ void updateTime()
 
 // =======================================================================
 long lastmillis=millis();
-void showTime(){
+void showData(){
   lcd.setCursor(6,1);
   String timeString = "";
   timeString+= h/10 ? h/10 : 0;
@@ -304,4 +198,9 @@ void showTime(){
   int clock_len = clock_time.length() + 1; 
   clock_time.toCharArray(time_message, clock_len) ;
   lcd.print(time_message);
+  String env = "T:" + String(temp)+"*c H:" + String(humidity)+"%";
+  lcd.setCursor(2,2);
+  lcd.print(env);
+  lcd.setCursor(3,3);
+  lcd.print(date);
 }
